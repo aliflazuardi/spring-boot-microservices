797e903edcfb6144ecce682b98cd15a08b938528
khalifah.l <khalifah.l@samsung.com>
2024-04-16 19:54:47 +0700

    add initial files for product service

---
+++ product-service/src/test/java/com/aliflazuardi/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliflazuardi.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

000e3c9cda3768f49c09987da47f7c59400a420e
khalifah.l <khalifah.l@samsung.com>
2024-04-23 11:42:04 +0700

    add Order Service initial files

---
+++ order-service/src/test/java/com/aliflazuardi/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliflazuardi.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

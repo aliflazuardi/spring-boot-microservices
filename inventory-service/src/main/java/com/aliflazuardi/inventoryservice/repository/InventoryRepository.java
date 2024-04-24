91adecafd31122a9d8c53a47455f198152d55854
aliflazuardi <aliflazuardi250999@gmail.com>
2024-04-24 15:03:58 +0700

    fix problem in repository not finding by skuCode

--- inventory-service/src/main/java/com/aliflazuardi/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/aliflazuardi/inventoryservice/repository/InventoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findBySkuCode();
+    Optional<Inventory> findBySkuCode(String skuCode);
 }

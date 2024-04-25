3c96793186228446f60176c74ef6c67add1869c1
aliflazuardi <aliflazuardi250999@gmail.com>
2024-04-25 08:10:55 +0700

    add helper code to insert data to database

--- inventory-service/src/main/java/com/aliflazuardi/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/aliflazuardi/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.aliflazuardi.inventoryservice;
 
+import com.aliflazuardi.inventoryservice.model.Inventory;
+import com.aliflazuardi.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,20 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	// only to insert data to database
+//	@Bean
+//	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+//		return args -> {
+//			Inventory inventory = new Inventory();
+//			inventory.setSkuCode("samsung_galaxy_s24_ultra");
+//			inventory.setQuantity(100);
+//
+//			Inventory inventory1 = new Inventory();
+//			inventory1.setSkuCode("samsung_galaxy_s24");
+//			inventory1.setQuantity(120);
+//
+//			inventoryRepository.save(inventory);
+//			inventoryRepository.save(inventory1);
+//		};
+//	}
 }

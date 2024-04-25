9d3b9956ca63744ed8c167a91ad4e5373a2c8e98
aliflazuardi <aliflazuardi250999@gmail.com>
2024-04-25 11:00:16 +0700

    fix inventory repository bug to return list of inventory

--- inventory-service/src/main/java/com/aliflazuardi/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/aliflazuardi/inventoryservice/repository/InventoryRepository.java
@@ -2,10 +2,9 @@ package com.aliflazuardi.inventoryservice.repository;
 
 import com.aliflazuardi.inventoryservice.model.Inventory;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findBySkuCodeIn(List<String> skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
